a71b872202f0564f8119019a9666e3a091b20b83
NagasaAnandes <nagasa.anandes@gmail.com>
2026-03-03 13:09:16 +0700

    Update docker V2

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,29 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use App\Models\JobVacancy;
 use App\Observers\JobVacancyObserver;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        JobVacancy::observe(JobVacancyObserver::class); {
-            if (app()->environment('production')) {
-                URL::forceScheme('https');
-            }
+        JobVacancy::observe(JobVacancyObserver::class);
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
         }
     }
 }
